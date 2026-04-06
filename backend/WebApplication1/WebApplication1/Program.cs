d1f90a31c3fad6a3a56d4e4260556d37a18a9216
mitchsmii <143842296+mitchsmii@users.noreply.github.com>
2026-04-06 01:02:07 -0600

    CORS changes

--- backend/WebApplication1/WebApplication1/Program.cs
+++ backend/WebApplication1/WebApplication1/Program.cs
@@ -19,7 +19,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:5173",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "https://intex-ochre.vercel.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
