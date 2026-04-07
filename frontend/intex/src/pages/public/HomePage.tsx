2b3a2f32c810c809ed1a7349d7625505c7c31598
mikelleburnett3 <mburn11@byu.edu>
2026-04-07 09:29:25 -0600

    convert png to webp

--- frontend/intex/src/pages/public/HomePage.tsx
+++ frontend/intex/src/pages/public/HomePage.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import shelterImg from '../../assets/shelter.png'
+import shelterImg from '../../assets/shelter.webp'
 import handsImg from '../../assets/hands.png'
 import './HomePage.css'
 
